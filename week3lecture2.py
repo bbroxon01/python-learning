93660ce615dec8ca5ce96c35a17903e5454da63e
Brandon <bbroxon01@indianatech.net>
2026-02-02 12:39:38 -0500

    In class progress

--- week3lecture2.py
+++ week3lecture2.py
@@ -15,4 +15,23 @@ print(type(f))
 g = 3 // 4 # expression - floor divison - results in integer - rounds towards minus infinity
 print(type(g))
 
-# stopped at 3.2
+# 3.2
+
+first = "Computer"
+second = "Science"
+full_course = first + " " + second
+separator = "----------"
+length = len(full_course)
+print(full_course)
+print(separator)
+print(length)
+
+# 3.3
+# This code has type-related errors. Fix them!
+quantity = input("How many items? ") # User enters: 3
+price = "9.99"
+subtotal = quantity * price
+tax_rate = 0.07
+tax = subtotal * tax_rate
+total = subtotal + tax
+print("Total: $" + total)
