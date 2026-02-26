f57be9fc11603a06b014880d999e185fa0d7d88f
Brandon <bbroxon01@indianatech.net>
2026-02-26 14:13:27 -0500

    Week 7 Exercises Done

--- week7.py
+++ week7.py
@@ -91,3 +91,110 @@ normal_list = [1, 2, 3, 4, 5]
 reversed_list = normal_list[::-1]
 print(normal_list) # Output: [1, 2, 3, 4, 5]
 print(reversed_list) # Output: [5, 4, 3, 2, 1]
+
+#Lecture 2
+
+#Unit 1 Exercises 
+# Exercise 1
+groceries = []
+groceries.append("Milk")
+print(groceries)
+groceries.append("Eggs")
+print(groceries)
+groceries.append("Bread")
+print(groceries)
+
+# Exercise 2
+numbers = [10, 30, 40]
+numbers.insert(1, 20) 
+numbers.insert(0, 5)
+numbers.append(50)
+numbers.extend([60, 70, 80])
+print(numbers)
+
+# Exercise 3
+a = [1,2]
+a.append([3,4])
+print(a)
+
+b = [1,2]
+b.extend([3,4])
+print(b)
+
+c = [1,2]
+c.append("hi")
+print(c)
+d = [1,2]
+d.extend("hi")
+print(d)
+
+# Unit 2 exercises
+# Exercise 1
+animals = ["cat", "dog", "bird", "dog", "fish"]
+animals.remove("bird")
+print(animals)
+last = animals.pop()
+print(last)
+print(animals)
+del animals[0]
+print(animals)
+
+# Exercise 2
+queue = ["Alice", "Bob", "Charlie", "David", "Eve"]
+queue.remove("David")
+served = queue.pop(0)
+print(f"{served} was served.")
+left = queue.pop()
+print(left)
+print(queue)
+
+# Exercise 3
+score = 100
+scores = [85, 92, 78, 64, 95, 88]
+if score in scores:
+    scores.remove(score)
+else: 
+    print(f"{score} not found.")
+
+index = 10
+if index < len(scores):
+    scores.pop(index)
+else:
+    print(f"{index} out of range.")
+    
+del scores[len(scores) // 2 - 1: len(scores) // 2 + 1]
+print(scores)
+
+# Unit 3 exercises
+# Exercise 1
+colors = ["red", "blue", "green", "yellow", "blue"]
+if "blue" in colors:
+    print("Blue is in the list.")
+else:
+    print("Blue is not in the list.")
+if "purple" not in colors:
+    print("Purple is not in the list.")
+else:
+    print("Purple is in the list.")
+
+print(f"Index of green: {colors.index('green')}")
+print(f"Blue occurs {colors.count('blue')} times")
+
+# Exercise 2
+students = ["Alice", "Bob", "Charlie", "David", "Alice"]
+student_name = input("Enter a student name: ")
+if student_name in students:
+    print(f"Index of {student_name}: {students.index(student_name)}")
+    print(f"{student_name} appears {students.count(student_name)} time.")
+else:
+    print(f"{student_name} is not enrolled.")
+
+# Exercise 3   
+inventory = ["hammer", "nails", "screwdriver", "nails", "wrench", "nails"]
+print(f"Nails are in inventory {inventory.count('nails')} times.")
+print(f"The first entry of nails is at index: {inventory.index('nails')}")
+inventory.remove('nails')
+print(f"The first entry of nails is now at index: {inventory.index('nails')}")
+inventory.remove('nails')
+print(f"Nails are now in inventory {inventory.count('nails')} time.")
+print(inventory)
