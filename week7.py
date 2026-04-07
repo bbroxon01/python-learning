d47a2a60b0a3ffa9ece2d5228097062024f86762
Brandon <bbroxon01@indianatech.net>
2026-04-07 11:02:00 -0400

    changes made

--- week7.py
+++ week7.py
@@ -3,13 +3,14 @@ list_b = list_a
 list_b[0] = 99
 print(list_a)  # Output: [99, 2, 3]
 
-# unit 1 beginner exercise
+#unit 1
+#beginner
 colors = ["red", "green", "blue"]
 numbers = [10, 20, 30]
 print(f'Colors: {colors}')
 print(f'Numbers: {numbers}\n Colors: {colors}')
 
-# unit 1 intermediate exercise
+#intermediate
 list_temp = [72, 75, 78]
 list_temp[0] = 70
 print(list_temp)  # Output: [70, 75, 78]
@@ -17,14 +18,15 @@ print(list_temp)  # Output: [70, 75, 78]
 word = "Hello"
 #word[0] = "J"  # This will raise an error because strings are immutable
 
-# unit 1 advanced exercise
+#advanced
 original = [10, 20, 30]
 alias = original
 alias[1] = 200
 print(original)  # Output: [10, 200, 30]
 #make an independent copy of the list
 
-# unit 2 beginner exercise
+# unit 2
+#beginner
 #create a list called animals with: "cat", "dog", "bird", "fish", and "hamster"
 
 animals = ["cat", "dog", "bird", "fish", "hamster"]
@@ -32,7 +34,7 @@ print(animals[0]) #cat
 print(animals[-1]) #hamster
 print(len(animals)) #5
 
-# unit 2 intermediate exercise
+#intermediate
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
 months[1] = "February"
 middle_index = len(months) // 2
@@ -41,7 +43,7 @@ print(months[middle_index]) #Apr
 print(months[-1]) #Jun
 print(len(months)-1) #5
 
-# unit 2 advanced exercise
+#advanced
 data = [100, 200, 300]
 empty = []
 if not empty:
@@ -51,7 +53,8 @@ else:
 data[0], data[-1] = data[-1], data[0]
 print(data)
 
-# unit 3 beginner exercise
+#unit 3
+#beginner
 numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90]
 #print first three numbers
 #print last three numbers
@@ -63,7 +66,7 @@ middle_index = len(numbers) // 2
 print(numbers[middle_index-1:middle_index+2])
 print(numbers[:])
 
-# unit 3 intermediate exercise
+#intermediate
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 #use slicing to:
 #1. get every other letter starting from "a"
@@ -75,7 +78,7 @@ print(alphabet[1::2])  # Output: ['b', 'd', 'f', 'h', 'j']
 print(alphabet[::-1])  # Output: ['j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
 print(alphabet[::-3])  # Output: ['j', 'g', 'd', 'a']
 
-# unit 3 advanced exercise
+#advanced
 course = "CS1300"
 course = list(course)
 course[3], course[4] = course[4], course[3]
@@ -94,7 +97,7 @@ print(reversed_list) # Output: [5, 4, 3, 2, 1]
 
 #Lecture 2
 
-#Unit 1 Exercises 
+#Unit 1 
 # Exercise 1
 groceries = []
 groceries.append("Milk")
@@ -128,7 +131,7 @@ d = [1,2]
 d.extend("hi")
 print(d)
 
-# Unit 2 exercises
+# Unit 2
 # Exercise 1
 animals = ["cat", "dog", "bird", "dog", "fish"]
 animals.remove("bird")
